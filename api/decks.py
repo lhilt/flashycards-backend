e7f995fed7a55f9de7e8e09d911a05da02f13588
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-12-03 15:32:10 -0800

    Add same controllers as CARDS to DECKS

---
+++ api/decks.py
@@ -0,0 +1,39 @@
+import json
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+
+from .models import Deck
+
+# Create your views here.
+## controllers for DECKS
+
+@require_http_methods(['GET'])
+def list_decks(request, user_pk):
+    decks = Deck.objects.filter(user=user_pk)
+    response = {'decks': list(decks.values())}
+    return JsonResponse(response, status=200)
+
+
+@require_http_methods(['POST'])
+def create_deck(request, user_pk):
+    new_deck_info = json.loads(request.body)
+    deck = Deck.objects.create(new_deck_info)
+    return JsonResponse({}, status=201)
+
+
+@require_http_methods(['PUT'])
+def edit_deck(request, user_pk, deck_pk):
+    deck = Deck.objects.get(pk=deck_pk)
+    new_info = json.loads(request.body)
+
+    Deck.objects.filter(pk=deck_pk).update(**new_info)
+    return JsonResponse(new_info, status=200)
+
+
+@require_http_methods(['DELETE'])
+def delete_deck(request, user_pk, deck_pk):
+    deck = Deck.objects.get(pk=deck_pk)
+    deck.delete()
+    return JsonResponse({}, status=200)
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import cards
+from . import cards, decks
 
 # current path: api/v1/
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path('users/<int:user_pk>/decks/<int:deck_pk>/cards', cards.create_card),
     path('users/<int:user_pk>/decks/<int:deck_pk>/cards', cards.edit_card),
     path('users/<int:user_pk>/decks/<int:deck_pk>/cards', cards.delete_card),
+    path('users/<int:user_pk>/decks', decks.list_decks),
+    path('users/<int:user_pk>/decks', decks.create_deck),
+    path('users/<int:user_pk>/decks/<int:deck_pk>', decks.edit_deck),
+    path('users/<int:user_pk>/decks/<int:deck_pk>', decks.delete_deck),
 ]
