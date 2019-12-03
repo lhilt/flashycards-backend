f87479f6eabc290a968916c92aab38733b98e1e9
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-12-03 13:41:28 -0800

    Add Deck and Card models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Deck(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="decks"
+    )
+
+
+class Card(models.Model):
+    front = models.TextField()
+    back = models.TextField()
+    deck = models.ForeignKey(
+        Deck,
+        on_delete=models.CASCADE,
+        related_name="cards"
+    )
