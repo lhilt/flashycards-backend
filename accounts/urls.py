26f1317811678f141bbb5c61b335f847430fe085
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-12-05 17:24:05 -0800

    Add view for logout; update views for signup, login with error msgs

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('token', views.sendToken),
     path('signup', views.signup),
+    path('login', views.login),
+    path('logout'), views.logout),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,16 +8,54 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
+def sendToken(request):
+    return JsonResponse({'message': 'welcome'}, status=200)
+
+
 @require_http_methods(['POST'])
-@csrf_exempt
 def signup(request):
     new_user_info = json.loads(request.body)
-    new_user_info.pop('password2')
-    user = User.objects.create_user(
-        **new_user_info,
-        first_name='',
-        last_name=''
+    username = new_user_info['username']
+    email = new_user_info['email']
+
+    errors = {}
+    if User.objects.filter(username=username).exists():
+        errors['username'] = 'Username is taken'
+    if User.objects.filter(email=email).exists():
+        errors['email'] = 'Invalid email'
+
+    if not errors:
+        new_user_info.pop('password2')
+        user = User.objects.create_user(
+            **new_user_info,
+            first_name='',
+            last_name=''
+        )
+        user.save()
+        auth.login(request, user)
+        return JsonResponse({}, status=200)
+    else:
+        return JsonResponse(errors, status=400)
+
+
+@require_http_methods(['POST'])
+def login(request):
+    user_info = json.loads(request.body)
+    username = user_info['username']
+    password = user_info['password']
+
+    user = auth.authenticate(
+        username=username,
+        password=password
     )
-    user.save()
-    auth.login(request, user)
-    return JsonResponse({}, status=200)
+    if user is not None:
+        auth.login(request, user)
+        return JsonResponse({'username': username}, status=200)
+    else:
+        error = {'error': 'Invalid credentials'}
+        return JsonResponse(error, status=400)
+
+
+def logout(request):
+    auth.logout(request)
+    return JsonResponse({'message': 'logout successful'}, status=200)
