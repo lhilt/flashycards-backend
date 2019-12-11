3e87ce1f04eabbe524115ccd1a881c466bf81edd
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-12-11 00:10:39 -0800

    Fix typo in allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['app.flashycards.lindseyhiltner.com']
+ALLOWED_HOSTS = ['api.flashycards.lindseyhiltner.com']
 
 
 # Application definition
