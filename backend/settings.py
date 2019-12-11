ac575d521011d4d807c2abe27db9512b2fad6a35
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-12-10 16:51:47 -0800

    Remove unused module

--- backend/settings.py
+++ backend/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from boto.s3.connection import S3Connection
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
