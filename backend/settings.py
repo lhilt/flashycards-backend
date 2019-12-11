9c2720c33f71efb2306ef02e375b46627ce3cb7b
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-12-10 22:41:53 -0800

    Remove CORS

--- backend/settings.py
+++ backend/settings.py
@@ -47,19 +47,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ORIGIN_WHITELIST = [
-    os.environ['REACT_APP_URL'],
-]
-
-CORS_ALLOW_CREDENTIALS = True
 
 SESSION_COOKIE_SAMESITE = None
 
